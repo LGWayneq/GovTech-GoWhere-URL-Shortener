7f8e2b371e2f1515222c2076c70fd73100436bb2
LGWayneq <g_wayneliau@yahoo.com.sg>
2023-02-13 21:27:02 +0800

    setup client and server folders

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>URL Shortener</h2>
+        <h4>Input your URL below:</h4>
+        <div className="input-container">
+          <input
+            className="url-input" />
+          <button
+            className="generate-button">
+            Generate
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
